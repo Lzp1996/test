71163eab1cab5febc2d1f87e237e8dd236c15c59
lzp1996 <1026583419@qq.com>
2026-07-03 01:16:23 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
+cat > src/App.vue << 'EOF'
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
-
 <template>
-  <h1 style="color: red;">Hello World</h1>
+  <h1 style="color: green">欢迎使用</h1>
   <HelloWorld />
 </template>
+EOF
